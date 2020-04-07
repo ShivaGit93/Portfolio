e2e6e1898cf87ed70e4ab79e204b3783b4b350ba
shivam <shivshopextra011@gmai.com>
2020-04-07 18:27:45 +0530

    AdditionofAllblogs.html

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import include
 import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
     path('', jobs.views.home,name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
