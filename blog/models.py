0c17c6258a1f25847340b8fe674e1fd12b7249c6
shivam <shivshopextra011@gmai.com>
2020-04-11 20:49:15 +0530

    AdditionofDetail

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Blog(models.Model):
         return self.titles
 
     def summary(self):
-        return self.body[:200]
+        return self.body[:1000]
     def pub_date_modified(self):
         return self.pub_date.strftime(' %b %e %Y')
 
